d164d6c885670aeea784df34c6b7068b87f8f104
silence <abaird0893@gmail.com>
2024-01-11 18:06:18 +0000

    added bindings for terminal mode

--- lua/silence/remap.lua
+++ lua/silence/remap.lua
@@ -6,7 +6,9 @@ vim.keymap.set("n", "m", "%")                                                   
 vim.keymap.set("n", "<leader>z", "zi")                                               -- toggle a fold
 
 
-vim.keymap.set("n", "<leader>ww", function() -- format and save file
+vim.keymap.set("n", "<leader>ss", vim.cmd.w)          -- save file
+vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format) -- format file
+vim.keymap.set("n", "<leader>ww", function()          -- format and save file
     vim.lsp.buf.format()
     vim.cmd("w")
 end)
@@ -27,6 +29,11 @@ vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle) -- toggle undotree
 
 -- Window/split navigation
 vim.keymap.set("n", "<leader><Tab>", vim.cmd.vsp) -- vertical split
+vim.keymap.set("n", "<leader><CR>", vim.cmd.sp)   -- split
+vim.keymap.set("n", "<A-j>", "<cmd>wincmd j<CR>") -- move to split down
+vim.keymap.set("n", "<A-k>", "<cmd>wincmd k<CR>") -- move to split up
+vim.keymap.set("n", "<A-h>", "<cmd>wincmd h<CR>") -- move to split left
+vim.keymap.set("n", "<A-l>", "<cmd>wincmd l<CR>") -- move to split right
 
 -- NVIM COMMENT
 vim.keymap.set("n", '<C-_>', ':CommentToggle<CR>')
@@ -34,8 +41,8 @@ vim.keymap.set("v", '<C-_>', ":'<,'>CommentToggle<CR>")
 
 -- TELESCOPE
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>fw', builtin.live_grep, {}) -- find words
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})     -- find files
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, {}) -- find words
 
 -- Bufferline tabbing
 local buf = require("bufferline")
@@ -52,3 +59,10 @@ vim.keymap.set("i", "<C-_>", "<Bslash>") -- rebind \ to CTRL /
 vim.keymap.set("v", "<C-C>", '"+y')          -- OS clipboard copy
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move block up
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move block down
+
+-- TERMINAL MODE REMAPS
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")       -- exit terminal mode
+vim.keymap.set("t", "<A-h>", "<C-\\><C-n><C-w>h") -- navigate from terminal mode to different window
+vim.keymap.set("t", "<A-j>", "<C-\\><C-n><C-w>j") -- navigate from terminal mode to different window
+vim.keymap.set("t", "<A-k>", "<C-\\><C-n><C-w>k") -- navigate from terminal mode to different window
+vim.keymap.set("t", "<A-l>", "<C-\\><C-n><C-w>l") -- navigate from terminal mode to different window

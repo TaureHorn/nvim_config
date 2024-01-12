a8c0bfeab7d2bcbb3b4cc065dca8e52f7098adc5
silence <abaird0893@gmail.com>
2024-01-12 10:41:31 +0000

    added in setup for cmp-path for directory autocomplete

--- after/plugin/lsp_autocomplete.lua
+++ after/plugin/lsp_autocomplete.lua
@@ -70,6 +70,6 @@ cmp.setup({
     }),
 
     sources = cmp.config.sources({
-        { name = 'nvim_lsp' }, { name = 'luasnip' } }, { { name = 'buffer' },
+        { name = 'nvim_lsp' }, { name = 'luasnip' } }, { { name = 'buffer' }, { name = 'path'},
     })
 })

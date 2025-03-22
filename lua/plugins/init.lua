9538430272ec76d444f0f683a34830eed04ea105
TaureHorn <6367950+TaureHorn@users.noreply.github.com>
2025-03-22 17:24:14 +0000

    formatting 2

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,6 @@
 return {
-
-
     -- COLORSCHEMES
     { 'eldritch-theme/eldritch.nvim', lazy = false, priority = 1000, opts = {}},
     { 'thedenisnikulin/vim-cyberpunk', lazy = false },
     { 'luisiacc/the-matrix.nvim', lazy = false },
-
 }

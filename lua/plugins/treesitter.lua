05acfba8172129a1a18014ad559e12577d0b44ff
TaureHorn <6367950+TaureHorn@users.noreply.github.com>
2025-03-24 13:32:01 +0000

    register handlebars for treesitter html highlights

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,9 @@ return {
     config = function()
         local configs = require("nvim-treesitter.configs")
 
+        -- setup highlighting for handlebars file
+        vim.cmd('autocmd BufRead,BufNewFile *.hbs set filetype=html')
+
         configs.setup({
             sync_install = false,
             auto_install = false,

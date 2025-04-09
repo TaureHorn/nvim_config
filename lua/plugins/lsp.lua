d13d111729b508a817a61e758cd0c5fc1b965571
TaureHorn <6367950+TaureHorn@users.noreply.github.com>
2025-04-09 20:45:07 +0100

    added lsps to nvim-lspconfig setup list

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,8 +7,11 @@ return {
         opts = {
             servers = {
                 bashls = {},
+                clangd = {},
                 cssls = {},
+                html = {},
                 gopls = {},
+                jsonls = {},
                 lua_ls = { settings = { Lua = { diagnostics = { globals = { 'vim' } } } } },
                 ts_ls = {},
             },
